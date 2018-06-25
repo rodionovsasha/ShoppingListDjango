103e7f3291c93230f398861fde1e3005e28ebca9
Rodionov Aleksandr <allrod@mail.ru>
2018-06-25 16:09:04 +0300

    init project

---
+++ shoppinglist/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class ItemsList(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    comment = models.CharField(max_length=255)
+    is_bought = models.BooleanField()
+    items_list = models.ForeignKey(ItemsList, on_delete=models.CASCADE, verbose_name="List of items")
+
+    def __str__(self):
+        return self.name
