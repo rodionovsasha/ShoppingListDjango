ace495aed60d02ad6cb7808f44db630b604541d3
Rodionov Aleksandr <allrod@mail.ru>
2018-06-26 10:11:21 +0300

    init view, template and static files

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')f@+(t893!l6nx#)tcae2w#s8o8uyp&*tja5%z0uz)e7uj-i14'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'rodionovsasha.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'rodionovsasha.pythonanywhere.com']
 
 
 # Application definition
@@ -119,4 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- shoppinglist/admin.py
+++ shoppinglist/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from .models import ItemsList
 from .models import Item
 
+
 admin.site.register(ItemsList)
 admin.site.register(Item)
