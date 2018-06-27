def34f6affc971a47cae5902bf44a7e6885efd50
Rodionov Aleksandr <allrod@mail.ru>
2018-06-27 09:53:46 +0300

    Add items list rendering

--- shoppinglist/views.py
+++ shoppinglist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import ItemsList
 
@@ -6,3 +6,9 @@ from .models import ItemsList
 def index(request):
     lists = ItemsList.objects.all()
     return render(request, 'shoppinglist/index.html', {'lists': lists})
+
+
+def items_list(request, id):
+    list_of_items = get_object_or_404(ItemsList, pk=id)
+    items = list_of_items.item_set.all().order_by('is_bought', 'name')
+    return render(request, 'shoppinglist/items_list.html', {'list': list_of_items, 'items': items})
